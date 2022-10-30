361c354cfe453ebb312ef846e560048e4aa04558
kossyak <akosyak@protonmail.com>
2022-10-30 22:51:27 +0400

    update

---
+++ index.js
@@ -0,0 +1,22 @@
+const collection = document.querySelector('.collection')
+const popup = document.querySelector('.popup')
+const close = document.querySelector('.close')
+const wr = document.querySelector('.wallpaper')
+const topBtn = document.querySelector('.top')
+
+collection.onclick = (event) => {
+  const img = event.target.closest('img')
+  if (img) {
+    popup.classList.add('popup__show')
+  }
+}
+close.onclick = () => {
+  popup.classList.remove('popup__show')
+}
+topBtn.onclick = () => {
+  wr.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  })
+}
