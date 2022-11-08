4f52f781846d7294df7d3e81c1ebfc030ecf50af
kossyak <akosyak@protonmail.com>
2022-11-08 14:11:07 +0400

    update

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const close = document.querySelector('.close')
 const wr = document.querySelector('.wallpaper')
 const topBtn = document.querySelector('.top')
 
-collection.onclick = (event) => {
-  const img = event.target.closest('img')
-  if (img) {
-    popup.classList.add('popup__show')
-  }
-}
-close.onclick = () => {
-  popup.classList.remove('popup__show')
-}
+// collection.onclick = (event) => {
+//   const img = event.target.closest('img')
+//   if (img) {
+//     popup.classList.add('popup__show')
+//   }
+// }
+// close.onclick = () => {
+//   popup.classList.remove('popup__show')
+// }
 topBtn.onclick = () => {
   wr.scrollTo({
     top: 0,
